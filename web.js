0765d920083c55751fe0a24a5362de8fb82c7b72
Clinberg <clinberg@yandex.ru>
2013-07-07 10:13:28 +0000

    Read index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var filename = 'index.html'
+var buf = fs.readFileSync(filename);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
